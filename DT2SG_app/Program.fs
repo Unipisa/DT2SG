1662ffae680367b1972a06093bc424b89bca3594
Guido <guido.scatena@unipi.it>
2019-07-14 00:39:21 +0200

    demo repository and some code

--- DT2SG_app/Program.fs
+++ DT2SG_app/Program.fs
@@ -7,39 +7,34 @@ open CommandLine
 open CommandLine.Text
 
 type options = {
-  [<Option('r', "root", Required = true, HelpText = "Path of the root of Directory Tree")>] root_string : string;
-  [<Option('g', "git", Required = false, HelpText = "Path of the git to start with")>] git_string : string;
-  //[<Option(HelpText = "Prints all messages to standard output.")>] verbose : bool;
-  //[<Option(Default = "русский", HelpText = "Content language.")>] language : string;
-  //[<Value(0, MetaName="offset", HelpText = "File offset.")>] offset : int64 option;
+  [<Option('r', "root", Required = true, HelpText = "Path of the root of Directory Tree")>] root_string: string;
+  [<Option('m', "metadata", Required = true, HelpText = "Path of the metadata file")>] metadata_string: string;
+  [<Option('i', "ignore", Required = true, HelpText = "Path of the ignore list file")>] ignore_string: string;
 }
 
 
 [<EntryPoint>]
 let main argv =
 
-    let mutable root_path = ""
-    let mutable git_path = ""
-    let run (a:CommandLine.Parsed<options>) = 
+    let mutable root_path, metadata_path, ignore_path = "", "", ""
+    let run (a: CommandLine.Parsed<options>) =
         root_path <- a.Value.root_string
-        if String.IsNullOrEmpty(a.Value.git_string) 
-            then git_path <- root_path + "/.git"
-            else git_path <- a.Value.git_string
-        //Console.WriteLine(a.Value.ToString())
+        metadata_path <- a.Value.root_string
+        ignore_path <- a.Value.root_string
 
-    let fail a = 
+    let fail a =
         Console.WriteLine(a.ToString())
 
 
     let result = CommandLine.Parser.Default.ParseArguments<options>(argv)
     match result with
-        | :? Parsed<options> as parsed ->  run parsed //.Value
+        | :? Parsed<options> as parsed -> run parsed //.Value
         | :? NotParsed<options> as notParsed -> fail notParsed.Errors
 
     //let root_path = "./demo/cmm"//argv.[0];
     //let root_path = argv.[0];
-   
-    Lib.createSyntheticGit(root_path, git_path)
+
+    Lib.createSyntheticGit(root_path, metadata_path, ignore_path)
     Console.WriteLine("You Git is in " + root_path + "/SGit/.git")
-    
+
     0 // return an integer exit code
--- DT2SG_lib/Library.fs
+++ DT2SG_lib/Library.fs
@@ -1,101 +1,87 @@
 ﻿namespace DT2SG_lib
 
-open System
-open System.IO
 open FSharp.Data
 open FSharp.Data.CsvExtensions
 open LibGit2Sharp
-open LibGit2Sharp
-
+open System
+open System.IO
 
 //TODO: 
 // *  handle and exclude symbolic links
 // *  handle directories that are not versions (how? replace in each revision ?)
-
 module Lib =
     // date format into author.csv
     let date_format = "MM'/'dd'/'yyyy HH':'mm':'ss"
 
     let rec directoryCopy srcPath dstPath copySubDirs =
-
         if not <| System.IO.Directory.Exists(srcPath) then
             let msg = System.String.Format("Source directory does not exist or could not be found: {0}", srcPath)
             raise (System.IO.DirectoryNotFoundException(msg))
-
-        if not <| System.IO.Directory.Exists(dstPath) then
-            System.IO.Directory.CreateDirectory(dstPath) |> ignore
-
+        if not <| System.IO.Directory.Exists(dstPath) then System.IO.Directory.CreateDirectory(dstPath) |> ignore
         let srcDir = new System.IO.DirectoryInfo(srcPath)
-
         for file in srcDir.GetFiles() do
             let temppath = System.IO.Path.Combine(dstPath, file.Name)
             file.CopyTo(temppath, true) |> ignore
-
         if copySubDirs then
             for subdir in srcDir.GetDirectories() do
                 let dstSubDir = System.IO.Path.Combine(dstPath, subdir.Name)
                 directoryCopy subdir.FullName dstSubDir copySubDirs
 
-    let hello name =
-        printfn "Hello %s" name
-          
-    let initGit(root_path, git_path)   =
-        Directory.CreateDirectory(git_path + "/../SGit") |> ignore
-        let gitPath =
-            if String.IsNullOrEmpty(git_path) 
-                then
-                    // if no existing git path given then create a new git    
-                    Repository.Init(root_path + "/../SGit")
-                else 
-                    // if existing git path given the try to use them
-                    let temp = Repository.Discover(git_path) //discover sale in su, forse meglio usare direttamente is valid
-                    if Repository.IsValid(temp) then temp else Repository.Init(root_path + "/../SGit")
+    let hello name = printfn "Hello %s" name
+
+    let initGit (root_path) =
+        // find git going up
+        let gitPath = Repository.Discover(root_path)
         gitPath
 
-    let commitVersionToGit(versionPath, message, author_name, author_handle, commit_date) =
+    let commitVersionToGit (versionPath, message, author_name, author_handle, commit_date) =
         let repo = new Repository(versionPath)
-        Commands.Stage(repo, "*");
+        Commands.Stage(repo, "*")
         // Create the committer's signature and commit
-        let author = Signature(author_name, author_handle, commit_date);
-        let committer = author;
+        let author = Signature(author_name, author_handle, commit_date)
+        let committer = author
         // Commit to the repository
-        let commit = repo.Commit(message, author, committer);
+        let commit = repo.Commit(message, author, committer)
         ()
 
-    let createSyntheticGit(root_path:string, git_path:string) =
-        let gitPath = initGit(root_path, git_path);
-        let csvFilename = root_path + "/../authors.csv"
-        let directories = 
-            Array.sort(Directory.GetDirectories(root_path)) 
-            |> Array.map (fun (a:string) -> a.Substring(root_path.Length + 1 , a.Length - (root_path.Length + 1) ))
-            //|> Array.filter (fuun (a:string) -> not(a = ".git")
-        let authorsAndDateStrings =CsvFile.Load(csvFilename).Cache()        
+    let createSyntheticGit (root_path: string, metadata_path: string, ignore_path: string) =
+        let git_path = initGit (root_path)
+        let authorsAndDateStrings = CsvFile.Load(metadata_path).Cache()
+        let IgnoreList = CsvFile.Load(ignore_path).Cache()
+        let directories =
+            Array.sort (Directory.GetDirectories(root_path))
+            |> Array.map (fun (a: string) -> a.Substring(root_path.Length + 1, a.Length - (root_path.Length + 1)))
         for dir in directories do
-            let finder = fun (row:CsvRow) -> (row.GetColumn "dir" = dir)  
-            let info =  Seq.tryFind finder (authorsAndDateStrings.Rows) 
-            let dest = Path.Combine(root_path,"./../SGit/WORKBENCH_TEMPLATE/SRC/")
-            let orig = Path.Combine(root_path, dir )
+            let finder = fun (row: CsvRow) -> (row.GetColumn "dir" = dir)
+            let info = Seq.tryFind finder (authorsAndDateStrings.Rows)
+            let dest = git_path.Replace("/.git","")
+            let orig = Path.Combine(root_path, dir)
             directoryCopy orig dest true
-            let none(a) = 
-                if String.IsNullOrWhiteSpace(a) then "n.d." else a
-            let author_name = if info.IsSome then info.Value.GetColumn "author_name" else none(null)
-            let author_handle = 
-                if info.IsSome  
-                    then 
-                        //let (handle:string) = (info.Value.GetColumn "author_github") 
-                        //if not(String.IsNullOrWhiteSpace(handle)) 
-                        //    then none("@" + handle) 
-                        //    else 
-                        none((info.Value.GetColumn "author_email"))
-                    else none(null)
-            let message = if info.IsSome then none(info.Value.GetColumn "message") else none(null)
-            let commit_date =
-                if info.IsSome
-                    then DateTimeOffset.ParseExact(info.Value.GetColumn "date", date_format, System.Globalization.CultureInfo.InvariantCulture)
-                    else DateTimeOffset.Now
-            Console.WriteLine("Commit dir: {0} with message : {1} on {2}", orig, message, commit_date.ToLocalTime().ToString())
-            commitVersionToGit(gitPath.Replace(".git/", ""), "V" + dir + " - " + message, author_name.TrimEnd(), author_handle.TrimEnd(), commit_date)
-            ()
-        done
+            let none (a) = if String.IsNullOrWhiteSpace(a) then "n.d." else a
+            let author_name = if info.IsSome then info.Value.GetColumn "author_name" else none (null)
+            let author_handle =
+                if info.IsSome then
+                    //let (handle:string) = (info.Value.GetColumn "author_github") 
+                    //if not(String.IsNullOrWhiteSpace(handle)) 
+                    //    then none("@" + handle) 
+                    //    else 
+                    none ((info.Value.GetColumn "author_email"))
+                else none (null)
 
+            let message =
+                if info.IsSome then none (info.Value.GetColumn "message")
+                else none (null)
+
+            let commit_date =
+                if info.IsSome then
+                    DateTimeOffset.ParseExact
+                        (info.Value.GetColumn "date", date_format, System.Globalization.CultureInfo.InvariantCulture)
+                else DateTimeOffset.Now
+
+            Console.WriteLine
+                ("Commit dir: {0} with message : {1} on {2}", orig, message, commit_date.ToLocalTime().ToString())
+            commitVersionToGit
+                (git_path.Replace(".git/", ""), "V" + dir + " - " + message, author_name.TrimEnd(),
+                 author_handle.TrimEnd(), commit_date)
+            ()
         ()
