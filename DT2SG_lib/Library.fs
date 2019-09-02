c8c19becf5e085bdac8dabb7bba161a3f18eb902
Guido Scatena <guido.scatena@unipi.it>
2019-09-02 12:22:52 +0200

    working with pre1970

--- DT2SG_lib/Library.fs
+++ DT2SG_lib/Library.fs
@@ -10,6 +10,10 @@ module Lib =
     // date format into author.csv
     let date_format = "MM'/'dd'/'yyyy HH':'mm':'ss"
 
+    let none (a) = if String.IsNullOrWhiteSpace(a) then "n.d." else a
+
+    let filter_git_files = fun (path: string) -> not(path.Contains(".git"))
+
     let rec directoryCopy srcPath dstPath copySubDirs =
         if not <| System.IO.Directory.Exists(srcPath) then
             let msg = System.String.Format("Source directory does not exist or could not be found: {0}", srcPath)
@@ -23,18 +27,7 @@ module Lib =
             for subdir in srcDir.GetDirectories() do
                 let dstSubDir = System.IO.Path.Combine(dstPath, subdir.Name)
                 directoryCopy subdir.FullName dstSubDir copySubDirs
-    let fixDateBefore1970(repo: Repository,author_date:DateTimeOffset) =
-        //fix dates as http://git.661346.n2.nabble.com/Back-dating-commits-way-back-for-constitution-git-td5365303.html#a5365346
-        //save commit object in a file, to be edited 
-        //$git cat-file -p HEAD > tmp.txt 
-        //edit tmp.txt, changing sign of author time
-        //$ [edit tmp.txt] 
-        //replace just created commit by handcrafted one 
-        //$ git reset --hard HEAD^ 
-        //$ git hash-object -t commit -w tmp.txt
-        //$ git update-ref -m 'commit: foo' refs/heads/master \ 
-        //fa5e5a2b6f27f10ce920ca82ffef07ed3eb3f26f 
-        ()
+
     let initGit (root_path,branch_name,committer_name, committer_email) =
         // find git going up
         let gitPath = Repository.Discover(root_path)
@@ -80,41 +73,68 @@ module Lib =
         )
         result
 
-    let fixPre1970Commit(author_date:DateTimeOffset, bad_date:DateTimeOffset, message, path) = 
+    let fixPre1970Commit(author_date:DateTimeOffset, bad_date:DateTimeOffset, message, path, branch) = 
+        //fix dates as http://git.661346.n2.nabble.com/Back-dating-commits-way-back-for-constitution-git-td5365303.html#a5365346
+        //save commit object in a file, to be edited 
+        //$git cat-file -p HEAD > tmp.txt 
+        //edit tmp.txt, changing sign of author time
+        //$ [edit tmp.txt] 
+        //replace just created commit by handcrafted one 
+        //$ git reset --hard HEAD^ 
+        //$ git hash-object -t commit -w tmp.txt
+        //$ git update-ref -m 'commit: foo' refs/heads/master \ 
+        //fa5e5a2b6f27f10ce920ca82ffef07ed3eb3f26f 
         let toReplace = bad_date.ToUnixTimeSeconds().ToString()
         let replaceWith = author_date.ToUnixTimeSeconds().ToString()
-        let out = executeInBash("git cat-file -p HEAD", path)
+        let mutable command = "git cat-file -p HEAD"
+        let out = executeInBash(command, path)
         let corrected = out.Replace(toReplace, replaceWith)
-        executeInBash("git reset --hard HEAD^ ", path) |> ignore
-        let hash = executeInBash("git hash-object -t commit -w " + corrected, path) //TODO: fix not correct read hash
-        executeInBash("git update-ref -m '" + message + "' refs/heads/master " + corrected, path) |> ignore
+        command <- "git reset --soft HEAD^ "
+        executeInBash(command, path) |> ignore
+        command <- "echo '" + corrected + "' | git hash-object -t commit -w --stdin "
+        let hash = executeInBash(command, path) 
+       
+        command <- "git update-ref -m '" + message + "' refs/heads/" + branch + " " + hash
+        let result = executeInBash(command, path) 
         ()
 
 
 
     let commitVersionToGit (
+                                // all files in master branch
                                 existing_files: Set<string>, 
+                                // current directory-version
                                 dir_to_add: string, 
-                                repo: Repository, 
-                                message, 
+                                //git repostiroy
+                                repo: Repository,
+                                // commit  
+                                message,
                                 author_name, 
                                 author_email, 
                                 author_date:DateTimeOffset, 
                                 tag, 
                                 committer_name, 
                                 committer_email, 
-                                commit_date, 
+                                commit_date,
+                                // used only in the last version to commit out of version files 
                                 ignore_files_to_commit,
+                                // name of the branch that will contains the versions
                                 branch_name,
+                                // path inside master branch of versions. ie /source when versions are /source/v1 ...
                                 relative_src_path,
+                                // true if first commit
                                 is_first_commit:bool
                                 ) =
+
         let filter_existing_files = (fun (file: string) -> not(existing_files.Contains(file)))
         let mutable src_branch = repo.Branches.[branch_name]
+        // move to branch containing versioning 
         Commands.Checkout(repo, src_branch) |> ignore
         let options = new CheckoutOptions()
-        options.CheckoutModifiers <- CheckoutModifiers.Force // { CheckoutModifiers = CheckoutModifiers.Force };
+        options.CheckoutModifiers <- CheckoutModifiers.Force 
+        // get the current version from master to src branch
         repo.CheckoutPaths("master", [relative_src_path + dir_to_add], options);
+        // if there are out of version files
         if not(Seq.isEmpty ignore_files_to_commit)
             then
                 for file in ignore_files_to_commit do
@@ -123,22 +143,20 @@ module Lib =
                         then System.IO.File.Copy(repo.Info.WorkingDirectory + relative_src_path + file, repo.Info.WorkingDirectory + "/" + file )
                         else directoryCopy (repo.Info.WorkingDirectory + relative_src_path + file) (repo.Info.WorkingDirectory) true
                 done
+        // move current version to the root of src branch        
         directoryCopy (repo.Info.WorkingDirectory + relative_src_path + dir_to_add) (repo.Info.WorkingDirectory) true
         let files_unstage =
                     Directory.GetFiles (repo.Info.WorkingDirectory + relative_src_path, "*.*", SearchOption.AllDirectories)
         System.IO.Directory.Delete(repo.Info.WorkingDirectory + relative_src_path, true)
-        //for f in  existing_files do  repo.Index.Remove(f) done
-
+        Commands.Unstage(repo, files_unstage)
+        
+        // stage current version files
         let files =
                     Directory.GetFiles (repo.Info.WorkingDirectory, "*.*", SearchOption.AllDirectories)
-                    //|> Array.filter filter_existing_files
-        //repo.Stage (files);
-        Commands.Unstage(repo, files_unstage)
+                    |> Array.filter filter_git_files
         Commands.Stage(repo, files)
-        // Create the committer's signature and commit
+        // Create the committer's signature
         let offset = author_date.ToUnixTimeSeconds()
-        //let d = DateTimeOffset.FromUnixTimeSeconds(offset)
-        //let question = (author_date = d )
         let author = Signature(author_name, author_email, author_date)
         let committer = Signature(committer_name, committer_email, commit_date)
         // Commit to the repository
@@ -149,18 +167,6 @@ module Lib =
                 let treeDefinition = new TreeDefinition()
                 let tree = repo.ObjectDatabase.CreateTree(treeDefinition)
                 last_commit <- repo.ObjectDatabase.CreateCommit(author, committer, message, tree, emptyCommit, false)
-                
-                ////////
-                //let year = last_commit.Author.When.Year
-                //let check = last_commit.Author.When.ToUnixTimeSeconds()
-                //let offset = author_date.ToUnixTimeSeconds()
-                //let d = DateTimeOffset.FromUnixTimeSeconds(check)
-                //let question1 = (check = offset )
-                //let question = (author_date = d )
-                //TODO: here the time signature is correct but the commit get wrong date if before 1/1/1970
-                //opened issue https://github.com/libgit2/libgit2sharp/issues/1707
-                //but related to libgit2
-                //maybe this is a workaround ? https://stackoverflow.com/questions/33640779/edit-old-commit-messages-using-libgit2sharpy
                 let master_branch =  repo.Branches.["master"]
                 Commands.Checkout(repo, master_branch) |> ignore
                 repo.Branches.Remove(src_branch)
@@ -173,7 +179,7 @@ module Lib =
             then
                 let bad_date = last_commit.Author.When 
                 let path = repo.Info.WorkingDirectory
-                fixPre1970Commit(author_date, bad_date, message, path)
+                fixPre1970Commit(author_date, bad_date, message, path, branch_name)
         let tag = repo.ApplyTag(tag);
        
         ()
@@ -186,48 +192,56 @@ module Lib =
     let createSyntheticGit (root_path: string, metadata_path: string, ignore_path: string, committer_name, committer_email) =
         let branch_name = "src"
         let git = initGit (root_path, branch_name, committer_name, committer_email)
-        let existing_files =
-                    Directory.GetFiles (git.Info.WorkingDirectory, "*.*", SearchOption.AllDirectories)
-                    |> Set.ofArray
-
         let git_path = git.Info.Path
         let relative_src_path = root_path.Replace(git_path.Replace("/.git", ""), "")
+        
+
+        let existing_files =
+                    Directory.GetFiles (git.Info.WorkingDirectory, "*.*", SearchOption.AllDirectories)
+                    |> Array.filter filter_git_files
+                    |> Set.ofArray
+
         let authorsAndDateStrings = CsvFile.Load(metadata_path).Cache()
+        
         let IgnoreList = CsvFile.Load(ignore_path).Cache().Rows
-        let ignore_files = Seq.map (fun (ignore_row: CsvRow) -> ignore_row.Columns.[0]) IgnoreList
-        let filter_ignore_dir = fun (row: string) ->
+        let ignore_files = 
+                    Seq.map (fun (ignore_row: CsvRow) -> ignore_row.Columns.[0]) IgnoreList
+
+        let directories =
+
+                let filter_ignore_dir = fun (row: string) ->
                                     not (Seq.exists (fun (ignore_row: CsvRow) ->
                                                         let dir = after_latest_slash row
                                                         dir = (ignore_row.Columns.[0])
                                                     )
                                 IgnoreList)
-        //TODO: better investigare how to handle symlink
-        ///https://stackoverflow.com/questions/1485155/check-if-a-file-is-real-or-a-symbolic-link
-        let filter_symbolic_links = fun (path: string) ->
+                //TODO: better investigare how to handle symlink
+                ///https://stackoverflow.com/questions/1485155/check-if-a-file-is-real-or-a-symbolic-link
+                let filter_symbolic_links = fun (path: string) ->
                                     let pathInfo = new FileInfo(path);
                                     not(pathInfo.Attributes.HasFlag(FileAttributes.ReparsePoint))
-        let directories =
+                in
                 Array.sort (Directory.GetDirectories(root_path))
                 |> Array.filter filter_ignore_dir
                 |> Array.filter filter_symbolic_links
+        
         let last_dir = after_latest_slash (Array.last directories)
         let mutable is_first_commit = true
+
         for dir in directories do
             let short_dir = after_latest_slash dir
-            let finder = fun (row: CsvRow) ->
-                            (row.GetColumn "dir" = short_dir)
-            let info = Seq.tryFind finder (authorsAndDateStrings.Rows)
+
+            let info = 
+                let finder = fun (row: CsvRow) -> (row.GetColumn "dir" = short_dir)
+                Seq.tryFind finder (authorsAndDateStrings.Rows)
+            
             let dest = git_path.Replace("/.git", "")
             let orig = Path.Combine(root_path, dir)
-            //directoryCopy orig dest true
-            let none (a) = if String.IsNullOrWhiteSpace(a) then "n.d." else a
+            
+
             let author_name = if info.IsSome then info.Value.GetColumn "author_name" else none (null)
             let author_handle =
                 if info.IsSome then
-                    //let (handle:string) = (info.Value.GetColumn "author_github") 
-                    //if not(String.IsNullOrWhiteSpace(handle)) 
-                    //    then none("@" + handle) 
-                    //    else yy
                     none ((info.Value.GetColumn "author_email"))
                 else none (null)
 
@@ -272,4 +286,6 @@ module Lib =
                     )
             is_first_commit <- false
             ()
+        // restore dicrectory to master 
+        Commands.Checkout(git, "master") |> ignore //TODO:genera conflitto
         ()
